17406695b6ddcc167ed5f1b892e4fb9c8e63d265
homerzhm <homer.zuo@inboxcube.com>
2016-05-22 12:57:38 -0400

    Some other file and change for share extension

---
+++ CodeTemplateShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  CodeTemplateShare
+//
+//  Created by Hongming Zuo on 2016-05-16.
+//  Copyright © 2016 Hongming Zuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
