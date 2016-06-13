743eed9df0e587d37e66a984e333cfa7929652a8
homerzhm <homer.zuo@inboxcube.com>
2016-06-13 00:03:45 -0400

    For helping spotlight

--- TemplateUtility/TemplateUtility.m
+++ TemplateUtility/TemplateUtility.m
@@ -7,6 +7,7 @@
 //
 
 #import "TemplateUtility.h"
+#import "AppDelegate.h"
 
 @implementation TemplateUtility
 
@@ -16,6 +17,7 @@ static TemplateUtility *instances = nil;
     @synchronized(self){
         if (instances == nil) {
             instances = [[self alloc]init];
+            [instances initialContetData];
         }
     }
     return instances;
@@ -29,8 +31,44 @@ static TemplateUtility *instances = nil;
     [result addObject:[self returnRouteTableDicWithName:@"UI Components" withControllerName:@"ComponentsTemplateController"]];
     [result addObject:[self returnRouteTableDicWithName:@"Key board Input" withControllerName:@"KeyboardInputViewController"]];
     [result addObject:[self returnRouteTableDicWithName:@"Item from Shared Extension" withControllerName:@"ShareExtensionTableViewController"]];
-    
+    [result addObject:[self returnRouteTableDicWithName:@"Spotlight items" withControllerName:@"SpolightViewController"]];
     return result;
+}
+
+-(void)initialContetData{
+    NSManagedObjectContext * context = [[TemplateUtility shareInstance] coredataManagerContext];
+    NSMutableArray * contents = [Content getAllContentWithContext:context];
+    if (contents.count == 0) {
+        NSArray * images = @[
+                             @{
+                                 @"Name":@"Niagara falls",
+                                 @"Description":@"Great falls......."
+                                 },
+                             @{
+                                 @"Name":@"Ontario Lake",
+                                 @"Description":@"Great lake......."
+                                 },
+                             @{
+                                 @"Name":@"Skating",
+                                 @"Description":@"ice...... great ice"
+                                 },
+                             @{
+                                 @"Name":@"Subway",
+                                 @"Description":@"Kobe... retired"
+                                 },
+                             @{
+                                 @"Name":@"Toronto",
+                                 @"Description":@"Beatufy place...."
+                                 }];
+        for (NSDictionary * dic in images) {
+            Content * newC = (Content *)[Content entityFromContext:context];
+            newC.name = [dic objectForKey:@"Name"];
+            newC.createDate = [NSDate date];
+            newC.contentDescription = [dic objectForKey:@"Description"];
+            newC.imagePath = [dic objectForKey:@"Name"];
+        }
+        [[TemplateUtility shareInstance] saveContext];
+    }
 }
 
 -(NSDictionary *)returnRouteTableDicWithName:(NSString *)name withControllerName:(NSString *)cName{
@@ -44,6 +82,20 @@ static TemplateUtility *instances = nil;
 
 -(NSURL *)getSharePathURL{
     return [[NSFileManager defaultManager]containerURLForSecurityApplicationGroupIdentifier:shareGroupIdentity];
+}
+
+-(NSMutableArray *)getContentArray{
+    NSMutableArray * result = [[NSMutableArray alloc]init];
+    
+    return result;
+}
+
+-(NSManagedObjectContext *)coredataManagerContext{
+    return [((AppDelegate *)[[UIApplication sharedApplication]delegate]) managedObjectContext];
+}
+
+-(void)saveContext{
+    [((AppDelegate *)[[UIApplication sharedApplication]delegate]) saveContext];
 }
 
 @end
