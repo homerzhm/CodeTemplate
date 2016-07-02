65a6e426ee756742242a23be8215b350cdd85cd3
homerzhm <homer.zuo@inboxcube.com>
2016-07-02 17:17:59 -0400

    For runtime study

---
+++ TemplateUtility/Category/TemplateUtility+TestMethodSwizzied.h
@@ -0,0 +1,13 @@
+//
+//  TemplateUtility+TestMethodSwizzied.h
+//  CodeTemplate
+//
+//  Created by Hongming Zuo on 2016-06-17.
+//  Copyright © 2016 Hongming Zuo. All rights reserved.
+//
+
+#import "TemplateUtility.h"
+
+@interface TemplateUtility (TestMethodSwizzied)
+
+@end
